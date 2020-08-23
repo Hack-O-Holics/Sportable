47f8e0a95f24508f05b6098311e5e3e8706a2e3e
agnik05 <agnik.banerjee@outlook.com>
2020-08-22 18:34:27 -0700

    Add Model Training Scripts

--- video-classifier/process_video.py
+++ video-classifier/process_video.py
@@ -8,114 +8,117 @@ import numpy as np
 
 
 def _raw_numpy_array(video_file, nframes=None):
-    """
-    Loads a video from the given file. Will set the number
-    of frames to `nframes` if this parameter is not `None`.
-    Returns:
-    - (width, height, arr): The width and height of the video,
-        and a numpy array with the parsed contents of the video.
-    """
+  """
+  Loads a video from the given file. Will set the number
+  of frames to `nframes` if this parameter is not `None`.
 
-    # Read video
-    cap = cv2.VideoCapture(video_file)
+  Returns:
+  - (width, height, arr): The width and height of the video,
+    and a numpy array with the parsed contents of the video.
+  """
 
-    # Get properties of the video
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+  # Read video
+  cap = cv2.VideoCapture(video_file)
 
-    # Min allowed height or width (whatever is smaller), in pixels
-    min_dimension = 256.0
+  # Get properties of the video
+  frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+  w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+  h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    # Determine scaling factors of width and height
-    assert min(w, h) > 0, 'Cannot resize {} with W={}, H={}'.format(video_file, w, h)
-    scale = min_dimension / min(w, h)
-    w = int(w * scale)
-    h = int(h * scale)
+  # Min allowed height or width (whatever is smaller), in pixels
+  min_dimension = 256.0
 
-    buf = np.zeros((1, frame_count, h, w, 3), np.dtype('float32'))
-    fc, flag = 0, True
+  # Determine scaling factors of width and height
+  assert min(w, h) > 0, 'Cannot resize {} with W={}, H={}'.format(video_file, w, h)
+  scale = min_dimension / min(w, h)
+  w = int(w * scale)
+  h = int(h * scale)
 
-    while fc < frame_count and flag:
-        flag, image = cap.read()
+  buf = np.zeros((1, frame_count, h, w, 3), np.dtype('float32'))
+  fc, flag = 0, True
 
-        if flag:
-            image = cv2.resize(image, (w, h))
-            buf[0, fc] = image
+  while fc < frame_count and flag:
+      flag, image = cap.read()
 
-        fc += 1
+      if flag:
+          image = cv2.resize(image, (w, h))
+          buf[0, fc] = image
 
-    cap.release()
+      fc += 1
 
-    if nframes is not None:
-        if nframes < frame_count:
-            fc = frame_count
-            t1, t2 = int(fc/2) - int(nframes/2), int(fc/2) + int(nframes/2)
-            buf = buf[:, t1:t2, :, :, :]
-        elif nframes > frame_count:
-            buf = np.resize(buf, (1, nframes, h, w, 3))
+  cap.release()
 
-    return w, h, buf
+  if nframes is not None:
+    if nframes < frame_count:
+      fc = frame_count
+      t1, t2 = int(fc/2) - int(nframes/2), int(fc/2) + int(nframes/2)
+      buf = buf[:, t1:t2, :, :, :]
+    elif nframes > frame_count:
+      buf = np.resize(buf, (1, nframes, h, w, 3))
+
+  return w, h, buf
 
 
 def _crop_video(numpy_video, size, desired_size):
-    """
-    Crop a video of the given size (WIDTH, HEIGHT) into a square of `desired_size`.
-    The video is represented as a numpy array. This func is for internal usage.
-    """
+  """
+  Crop a video of the given size (WIDTH, HEIGHT) into a square of `desired_size`.
+  The video is represented as a numpy array. This func is for internal usage.
+  """
 
-    w, h = size
-    h1, h2 = int(h/2) - int(desired_size/2), int(h/2) + int(desired_size/2)
-    w1, w2 = int(w/2) - int(desired_size/2), int(w/2) + int(desired_size/2)
-    return numpy_video[:, :, h1:h2, w1:w2, :]
+  w, h = size
+  h1, h2 = int(h/2) - int(desired_size/2), int(h/2) + int(desired_size/2)
+  w1, w2 = int(w/2) - int(desired_size/2), int(w/2) + int(desired_size/2)
+  return numpy_video[:, :, h1:h2, w1:w2, :]
 
 
 def rgb_data(video_file, size, nframes=None):
-    """
-    Loads a numpy array of shape (1, nframes, size, size, 3) from a video file.
-    Values contained in the array are based on RGB values of each frame in the video.
-    Parameter `size` should be an int (pixels) for a square cropping of the video.
-    Omitting the parameter `nframes` will preserve the original # frames in the video.
-    """
+  """
+  Loads a numpy array of shape (1, nframes, size, size, 3) from a video file.
+  Values contained in the array are based on RGB values of each frame in the video.
 
-    # Load video into numpy array
-    w, h, buf = _raw_numpy_array(video_file, nframes=nframes)
+  Parameter `size` should be an int (pixels) for a square cropping of the video.
+  Omitting the parameter `nframes` will preserve the original # frames in the video.
+  """
 
-    # Scale pixels between -1 and 1
-    buf[0, :] = ((buf[0, :] / 255.0) * 2) - 1
+  # Load video into numpy array
+  w, h, buf = _raw_numpy_array(video_file, nframes=nframes)
 
-    # Select center crop from the video
-    return _crop_video(buf, (w, h), size)
+  # Scale pixels between -1 and 1
+  buf[0, :] = ((buf[0, :] / 255.0) * 2) - 1
+
+  # Select center crop from the video
+  return _crop_video(buf, (w, h), size)
 
 
 def flow_data(video_file, size, nframes=None):
-    """
-    Loads a numpy array of shape (1, nframes, size, size, 2) from a video file.
-    Values contained in the array are based on optical flow of the video.
-    https://docs.opencv.org/3.1.0/d6/d39/classcv_1_1cuda_1_1OpticalFlowDual__TVL1.html
-    Parameter `size` should be an integer (pixels) for a square cropping of the video.
-    Omitting the parameter `nframes` will preserve the original # frames in the video.
-    """
+  """
+  Loads a numpy array of shape (1, nframes, size, size, 2) from a video file.
+  Values contained in the array are based on optical flow of the video.
+  https://docs.opencv.org/3.1.0/d6/d39/classcv_1_1cuda_1_1OpticalFlowDual__TVL1.html
 
-    # Load video into numpy array, and crop the video
-    w, h, buf = _raw_numpy_array(video_file, nframes=nframes)
-    buf = _crop_video(buf, (w, h), size)
+  Parameter `size` should be an integer (pixels) for a square cropping of the video.
+  Omitting the parameter `nframes` will preserve the original # frames in the video.
+  """
 
-    num_frames = buf.shape[1]
-    flow = np.zeros((1, num_frames, size, size, 2), dtype='float32')
+  # Load video into numpy array, and crop the video
+  w, h, buf = _raw_numpy_array(video_file, nframes=nframes)
+  buf = _crop_video(buf, (w, h), size)
 
-    # Convert to grayscale
-    buf = np.dot(buf, np.array([0.2989, 0.5870, 0.1140]))
+  num_frames = buf.shape[1]
+  flow = np.zeros((1, num_frames, size, size, 2), dtype='float32')
 
-    # Apply optical flow algorithm
-    for i in range(1, num_frames):
-        prev, cur = buf[0, i - 1], buf[0, i]
-        cur_flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+  # Convert to grayscale
+  buf = np.dot(buf, np.array([0.2989, 0.5870, 0.1140]))
 
-        # Truncate values to [-20, 20] and scale from [-1, 1]
-        cur_flow[cur_flow < -20] = -20
-        cur_flow[cur_flow > 20] = 20
-        cur_flow /= 20
-        flow[0, i] = cur_flow
+  # Apply optical flow algorithm
+  for i in range(1, num_frames):
+      prev, cur = buf[0, i - 1], buf[0, i]
+      cur_flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-    return flow
+      # Truncate values to [-20, 20] and scale from [-1, 1]
+      cur_flow[cur_flow < -20] = -20
+      cur_flow[cur_flow > 20] = 20
+      cur_flow /= 20
+      flow[0, i] = cur_flow
+
+  return flow
